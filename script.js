1c3ce6719d8bb0eab074ddc2e420be8f731264c5
approved4me <approved4me@gmail.com>
2023-02-02 14:38:52 +0100

    javascript defer atributte

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-const form = document.querySelector(".rating-form");
-const rateStart = document.querySelector(".content_rating");
-const thankYou = document.querySelector(".content_thanks");
-const value = document.getElementById("rating-value");
+const form = document.querySelector(".card_holder");
+const thankYou = document.querySelector(".thank_you");
+
 
 form.addEventListener("submit", function(e) {
   e.preventDefault(); //prevents Browser from refreshing
-  const selector = "input[name=ratings]:checked";
-  const checkedInput = document.querySelector(selector);
-  rateStart.classList.add("hidden");
+  form.classList.add("hidden");
   thankYou.classList.remove("hidden");
-  value.innerHTML = checkedInput.value;
-  console.log(checkedInput.value);
+  
 })
